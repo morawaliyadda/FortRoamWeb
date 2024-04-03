1e696c475b147195998e54c34806c6156154191a
morawaliyadda <sithara.morawaliyadda2000@gmail.com>
2024-04-03 13:50:21 +0530

    Update HistoricalPlaces.jsx

--- src/screens/HistoricalPlaces.jsx
+++ src/screens/HistoricalPlaces.jsx
@@ -1,126 +1,29 @@
-
 // import React from "react";
 // import "../App.css";
 // import "../components/CardStyle/CardStyle.css";
 // import CardData from "../components/CardStyle/CardStyle";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-// import data from "../components/places/places.jsx"; 
-
-// const NextArrow = (props) => {
-//     const { onClick } = props;
-//     return (
-//         <div className="slick-arrow slick-next " onClick={onClick}>
-//             <IoIosArrowForward/>
-//         </div>
-//     );
-// };
-
-// const PrevArrow = (props) => {
-//     const { onClick } = props;
-//     return (
-//         <div className="slick-arrow slick-prev" onClick={onClick}>
-//             <IoIosArrowBack/>
-//         </div>
-//     );
-// };
-
-// const HistoricalPlaces = () => {
-//     const settings = {
-//         nextArrow: <NextArrow />,
-//         prevArrow: <PrevArrow />,
-//         infinite: true,
-//         speed: 500,
-//         slidesToShow: 4,
-//         slidesToScroll: 1,
-//         swipeToSlide: true,
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                 },
-//             },
-//         ],
-//     };
-
-//     const groupedPlaces = data.place.reduce((acc, place) => {
-//         if (place.type === 'historical') {
-//             if (!acc[place.subtype]) {
-//                 acc[place.subtype] = [];
-//             }
-//             acc[place.subtype].push(place);
-//         }
-//         return acc;
-//     }, {});
-
-//     const capitalizeFirstLetter = (string) => {
-//         return string.charAt(0).toUpperCase() + string.slice(1);
-//     };
-
-//     return (
-//         <div className="Categorical-place">
-//             <h1 className="Categorical-heading">Historical <strong>Insights</strong> </h1>
-//             <div className="Categorical-type">
-//                 {Object.entries(groupedPlaces).map(([subtype, places]) => (
-//                     <div key={subtype}>
-//                         <h2 className="subtype">{capitalizeFirstLetter(subtype)}</h2>
-//                         <Slider {...settings}>
-//                             {places.map((place, index) => (
-//                                 <div key={index}>
-//                                     <CardData
-//                                         image={require(`../assets/placeImages/${place.image}`)}
-//                                         heading={place.title}
-//                                         location= {place.street}
-//                                         description={place.description}
-//                                         review ={place.review}
-                                        
-//                                     />
-//                                 </div>
-//                             ))}
-//                         </Slider>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default HistoricalPlaces;
-
-
-
-// import React from "react";
-// import "../App.css";
-// import "../components/CardStyle/CardStyle.css";
-// import CardData from "../components/CardStyle/CardStyle";
-// import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 // import data from "../components/places/places.jsx";
-// import HorizontalScroll from 'react-horizontal-scrolling-menu';
-// import { ScrollMenu } from 'react-horizontal-scrolling-menu';
 
-
-// const NextArrow = (props) => {
-//     const { onClick } = props;
-//     return (
-//         <div className="slick-arrow slick-next " onClick={onClick}>
-//             <IoIosArrowForward/>
-//         </div>
-//     );
-// };
-
-// const PrevArrow = (props) => {
-//     const { onClick } = props;
-//     return (
-//         <div className="slick-arrow slick-prev" onClick={onClick}>
-//             <IoIosArrowBack/>
-//         </div>
-//     );
-// };
 
 // const HistoricalPlaces = () => {
+
+//     const [places, setPlaces] = useState([]);
+
+//         useEffect(() => {
+    
+//             fetch("http://localhost:3010/place/type/historical")
+//                 .then(response => response.json())
+//                 .then(data => {
+    
+//                     if (Array.isArray(data)) {
+//                         setPlaces(data);
+//                     } else {
+//                         console.error("Data fetched is not an array:", data);
+//                     }
+//                 })
+//                 .catch(error => console.error("Error fetching places:", error));
+//         }, []);
+
 //     const groupedPlaces = data.place.reduce((acc, place) => {
 //         if (place.type === 'historical') {
 //             if (!acc[place.subtype]) {
@@ -139,21 +42,30 @@
 //         <div className="Categorical-place">
 //             <h1 className="Categorical-heading">Historical <strong>Insights</strong> </h1>
 //             <div className="Categorical-type">
-//                 {Object.entries(groupedPlaces).map(([subtype, places]) => (
-//                     <div key={subtype}>
-//                         <h2 className="subtype">{capitalizeFirstLetter(subtype)}</h2>
-//                         <ScrollMenu>
-//                             {places.map((place, index) => (
-//                                 <CardData
-//                                     key={index}
-//                                     image={require(`../assets/placeImages/${place.image}`)}
-//                                     heading={place.title}
-//                                     location={place.street}
-//                                     description={place.description}
-//                                     review={place.review}
-//                                 />
-//                             ))}
-//                         </ScrollMenu>
+
+        //    {Object.entries(groupedPlaces).map(([subtype, places]) => (
+        //             <div key={subtype}>
+        //                 <h2 className="subtype">{capitalizeFirstLetter(subtype)}</h2>
+        //                 <div className="choose-item">
+        //                     {places.map((place, index) => (
+        //                         <div key={index}>
+        //                             <CardData
+        //                                // image={require(`../assets/placeImages/${place.image}`)}
+        //                                 heading={place.title}
+        //                                 location={place.street}
+        //                                 description={place.description}
+        //                                 review={place.review}
+
+        //                             />
+        //                         </div>
+        //                     ))}
+        //                 </div>
+
+        //             </div>
+        //         ))}
+
+
+//                         </div>
 //                     </div>
 //                 ))}
 //             </div>
@@ -167,16 +79,9 @@
 
 
 import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import CardData from "../components/CardStyle/CardStyle";
-
-import React from "react";
 import "../App.css";
 import "../components/CardStyle/CardStyle.css";
-import CardData from "../components/CardStyle/CardStyle";
- 
-
 
 
 const HistoricalPlaces = () => {
@@ -184,11 +89,11 @@ const HistoricalPlaces = () => {
     const [places, setPlaces] = useState([]);
 
     useEffect(() => {
-    
+
         fetch("http://localhost:3010/place/type/historical")
             .then(response => response.json())
             .then(data => {
-               
+
                 if (Array.isArray(data)) {
                     setPlaces(data);
                 } else {
@@ -204,23 +109,7 @@ const HistoricalPlaces = () => {
         return acc;
     }, {});
 
-    const settings = {
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        swipeToSlide: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                },
-            },
-        ],
-    };
+    
 
 
     return (
@@ -228,81 +117,92 @@ const HistoricalPlaces = () => {
             <h1 className="Categorical-heading">Historical <strong>Insights</strong> </h1>
             <div className="Categorical-type">
 
-                {/* <Slider {...settings}> */}
-                    {/* Render museums */}
-                  
-                    {groupedPlaces["museum"] && groupedPlaces["museum"].map((place, index) => (
+              
+                {/* Render museums */}
+                <h2 className="subtype">Museum</h2>
+                <div className="choose-item">
+                {groupedPlaces["museum"] && groupedPlaces["museum"].map((place, index) => (
+
+                    <div key={index}>
+                       
+
+                        <CardData
+                            // image={place.image}
+                            heading={place.title}
+                            location={place.street}
+                            description={place.description}
+                            review={place.review}
+                        />
+
+                    </div>
+                ))}
+                </div>
+
+                {/* Render bastions */}
+                <h2 className="subtype">Bastion</h2>
+                <div className="choose-item">
+                {groupedPlaces["bastion"] && groupedPlaces["bastion"].map((place, index) => (
+                    <div key={index}>
                         
-                        <div key={index}>
-                            <h2 className="subtype">Museum</h2>
-                            
-                            <CardData
-                                // image={place.image}
-                                heading={place.title}
-                                location={place.street}
-                                description={place.description}
-                                review={place.review}
-                            />
-                           
-                        </div>
-                    ))}
-                  
-                   
-                    {/* Render bastions */}
-                    {groupedPlaces["bastion"] && groupedPlaces["bastion"].map((place, index) => (
-                        <div key={index}>
-                            <h2 className="subtype">Bastion</h2>
-                            
-                            <CardData
-                                // image={place.image}
-                                heading={place.title}
-                                location={place.street}
-                                description={place.description}
-                                review={place.review}
-                            />
-                           
-                        </div>
-                    ))}
-                   
-                   
-                    {/* Render others */}
-                    {groupedPlaces["others"] && groupedPlaces["others"].map((place, index) => (
-                        <div key={index}>
-                            <h2 className="subtype">Others</h2>
-                           
-                            <CardData
-                                // image={place.image}
-                                heading={place.title}
-                                location={place.street}
-                                description={place.description}
-                                review={place.review}
-                            />
-                          
-                        </div>
-                    ))}
-                    
+
+                        <CardData
+                            // image={place.image}
+                            heading={place.title}
+                            location={place.street}
+                            description={place.description}
+                            review={place.review}
+                        />
+
+                    </div>
+                ))}
+                </div>
+
+
+                {/* Render others */}
+                <h2 className="subtype">Others</h2>
+                <div className="choose-item">
+                {groupedPlaces["others"] && groupedPlaces["others"].map((place, index) => (
+                    <div >
+                     
+                      <div className="choose-item">
+                    <div key={index}>
+                      
+
+                        <CardData
+                            // image={place.image}
+                            heading={place.title}
+                            location={place.street}
+                            description={place.description}
+                            review={place.review}
+                        />
+                    </div>
+                    </div>
+                    </div>
+                ))}
+                </div>
+                
                 {/* </Slider> */}
 
-                {Object.entries(groupedPlaces).map(([subtype, places]) => (
+                {/* {Object.entries(groupedPlaces).map(([subtype, places]) => (
                     <div key={subtype}>
                         <h2 className="subtype">{capitalizeFirstLetter(subtype)}</h2>
                         <div className="choose-item">
-                        {places.map((place, index) => (
+                            {places.map((place, index) => (
                                 <div key={index}>
                                     <CardData
-                                        image={require(`../assets/placeImages/${place.image}`)}
+                                       // image={require(`../assets/placeImages/${place.image}`)}
                                         heading={place.title}
-                                        location= {place.street}
+                                        location={place.street}
                                         description={place.description}
-                                        review ={place.review}
-                                        
+                                        review={place.review}
+
                                     />
                                 </div>
                             ))}
                         </div>
 
                     </div>
-                ))}
+                ))} */}
 
             </div>
         </div>
