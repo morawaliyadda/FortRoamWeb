d3dc160234c853d6ba0246cd1362f11925f7abc0
Imesh <imeshthanapathi@gmail.com>
2024-05-17 17:16:28 +0530

    PopularPlaces image path changed

--- src/components/PopularPlaces/PopularPlaces.jsx
+++ src/components/PopularPlaces/PopularPlaces.jsx
@@ -96,7 +96,7 @@ const PopularPlaces = () => {
                     <CardData
                         key={index}
                        // image={require(`../../assets/placeImages/${place.image}`)}
-                        image= {`http://localhost:3010/src/${place.image}`} 
+                        image= {place.image}
                         heading={place.title}
                         location={place.street}
                         description={truncateDescription(place.description, 100)}
